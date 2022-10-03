9107b39bd2cfef55643f31d49a7262c978b5e12f
kyslegion <kyslegion@gmail.com>
2022-10-03 12:02:58 +0200

    cata

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var express = require('express');
 var app = express();
 const path =require('path');
 
+let port=4000;
+app.listen(port,()=>{
+  console.log(port);
+});
+
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname+ '/'));
@@ -21,13 +26,10 @@ app.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname, './connexion.html'));
 });
 // about page
-// app.get('/catalogue', function(req, res) {
-//   res.render('catalogue.ejs');
-// });
+app.get('/catalogue', function(req, res) {
+  res.render('catalogue.ejs');
+  res.sendFile(path.join(__dirname, './zobi.html'));
+});
 // app.listen('3000',()=>{
 //     console.log("Serveur 3000");
 // })
-let port=4000;
-app.listen(port, '0.0.0.0',()=>{
-  console.log(port);
-});
