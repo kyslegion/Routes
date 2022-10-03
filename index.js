8b77af61623770b685076a30caa888cdecab4dfd
kyslegion <kyslegion@gmail.com>
2022-10-03 12:59:22 +0200

    l

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ app.use(express.static(__dirname+ '/'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(__dirname + '/views'));
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'))
 
 // use res.render to load up an ejs view file
 //l
