99a14d8f44bb3fdb112b165cd04a9b18ca936183
kyslegion <kyslegion@gmail.com>
2022-10-03 10:52:16 +0200

    v8

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 var express = require('express');
 var app = express();
+const path =require('path');
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname+ '/'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -9,10 +14,12 @@ app.set('view engine', 'ejs');
 //l
 
 // index page
-app.get('/', function(req, res) {
-  res.render('index.ejs');
+// app.get('/', function(req, res) {
+//   res.render('index.ejs');
+// });
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname, './connexion.html'));
 });
-
 // about page
 app.get('/catalogue', function(req, res) {
   res.render('catalogue.ejs');
